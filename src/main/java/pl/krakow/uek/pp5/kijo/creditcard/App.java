84db192d73486cf317162c478253cddc5c0e9e37
kijo <kamil31@interia.eu>
2019-10-24 10:37:11 +0200

    test dodania programu

--- src/main/java/pl/krakow/uek/pp5/kijo/creditcard/App.java
+++ src/main/java/pl/krakow/uek/pp5/kijo/creditcard/App.java
@@ -2,6 +2,6 @@ package pl.krakow.uek.pp5.kijo.creditcard;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("elo");
+        System.out.println("elo ds");
     }
 }
